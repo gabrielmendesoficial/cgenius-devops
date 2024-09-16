7114c178b97397c9691393cfbe0a396590f8fcea
MarcosHGdaSilva <henriquegarrido1998@gmail.com>
2024-09-15 23:20:27 -0300

    adicionando cors em login

--- src/main/java/br/com/fiap/cgenius/auth/AuthController.java
+++ src/main/java/br/com/fiap/cgenius/auth/AuthController.java
@@ -1,9 +1,11 @@
 package br.com.fiap.cgenius.auth;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = {"*"}, maxAge = 3600)
 @RestController
 public class AuthController {
     private final AuthService authService;
